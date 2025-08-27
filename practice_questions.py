1ab0d88518a97b594a560e52ab1d9f972d72fa53
fatimaaziz27 <fatimaaziz250907@gmail.com>
2025-08-27 08:54:50 +0500

    third commit

--- practice_questions.py
+++ practice_questions.py
@@ -138,3 +138,123 @@ while True:
 # do you have any question?  2 question
 # do you have any question?  3 question
 # do you have any question?  Bye 
+
+#Q) Print seating chart for bus (Rows 1–3, Seats A–D).
+
+#CODE:
+
+rows= range(1, 4)
+seats = ['A', 'B','C','D']
+for row in rows:
+    for seat in seats:
+        print(f"{row}{seat}")
+
+#OUTPUT:
+
+#1A
+#1B
+#1C
+#1D
+#2A
+#2B
+#2C
+#2D
+#3A
+#3B
+#3C
+#3D
+
+#Q) Nested loop to find all team pairs from player list. 
+
+#CODE:
+
+players = ['Player1', 'Player2', 'Player3', 'Player4']
+for i in range(len(players)):
+    for j in range(i + 1, len(players)):
+        print(f"Team: {players[i]} vs {players[j]}")
+
+#OUTPUT:
+
+#Team: Player1 vs Player2
+#Team: Player1 vs Player3
+#Team: Player1 vs Player4
+#Team: Player2 vs Player3
+#Team: Player2 vs Player4
+#Team: Player3 vs Player4
+
+#Q) Generate full multiplication tables from 2–5. 
+
+#CODE:
+
+i=2
+while i <= 5:
+    print(f"TABLE OF {i}")
+    j=1
+    while j<=10:
+        print(f"{i} x {j} = {i*j}")
+        j+=1
+    print()
+    i+=1    
+
+#OUTPUT:
+
+#TABLE OF 2
+#2 x 1 = 2
+#2 x 2 = 4
+#2 x 3 = 6
+#2 x 4 = 8
+#2 x 5 = 10
+#2 x 6 = 12
+#2 x 7 = 14
+#2 x 8 = 16
+#2 x 9 = 18
+#2 x 10 = 20
+
+#TABLE OF 3
+#3 x 1 = 3
+#3 x 2 = 6
+#3 x 3 = 9
+#3 x 4 = 12
+#3 x 5 = 15
+#3 x 6 = 18
+#3 x 7 = 21
+#3 x 8 = 24
+#3 x 9 = 27
+#3 x 10 = 30
+
+#TABLE OF 4
+#4 x 1 = 4
+#4 x 2 = 8
+#4 x 3 = 12
+#4 x 4 = 16
+#4 x 5 = 20
+#4 x 6 = 24
+#4 x 7 = 28
+#4 x 8 = 32
+#4 x 9 = 36
+#4 x 10 = 40
+
+#TABLE OF 5
+#5 x 1 = 5
+#5 x 2 = 10
+#5 x 3 = 15
+#5 x 4 = 20
+#5 x 5 = 25
+#5 x 6 = 30
+#5 x 7 = 35
+#5 x 8 = 40
+#5 x 9 = 45
+#5 x 10 = 50
+
+#Q) Function to count how many `.pdf` files are in a list. 
+
+#CODE:
+
+files = ['file1.pdf', 'file2.txt', 'file3.pdf', 'file4.docx','file5.pdf','file6.pdf']
+def pdf__count():
+    count = len([file for file in files if file.endswith('.pdf')])
+    print(count)
+pdf__count()
+
+#OUTPUT:  4
+
