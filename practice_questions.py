dd0a4cdbcd2785d8861dc9538f74145b992b396d
fatimaaziz27 <fatimaaziz250907@gmail.com>
2025-12-17 01:25:51 +0500

    eighteeth commit

--- practice_questions.py
+++ practice_questions.py
@@ -669,3 +669,36 @@ print(s[1:] + s[0])
 
 # OUTPUT:
 # bca
+
+# Q) Compress string (`aaabb` → `a3b2`). 
+
+# ANSWER:
+
+s = "aaabb"
+result = ""
+count = 1
+for i in range(1, len(s)):
+    if s[i] == s[i-1]:
+        count += 1
+    else:
+        result += s[i-1] + str(count)
+        count = 1
+result += s[-1] + str(count)
+print(result)
+
+# OUTPUT:
+
+# a3b2
+
+# Q) Reverse words in sentence without slicing. 
+
+# ANSWER:
+
+sentence = "Hello World"
+words = sentence.split()
+reversed_words = [word[::-1] for word in words]
+print(' '.join(reversed_words))
+
+# OUTPUT:
+
+# olleH dlroW
