10ad5c4bbc413e48db56cb0c6b5caf2875e32f50
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-01-03 19:50:01 +0500

    commit

---
+++ VARIABLES.py
@@ -0,0 +1,63 @@
+#                          Variables:
+# Q) What will be the output of the following code?
+
+a = 5
+b = 10
+c = a + b
+print(c)
+
+# OUTPUT:
+# 15
+
+# Q) What will be the output of the following code?
+
+num = 42
+text = "hello"
+flag = True
+print(type(num))
+print(type(text))
+print(type(flag))
+
+# OUTPUT:
+# <class 'int'>
+# <class 'str'>
+# <class 'bool'>
+
+# Q) What will be the output of the following code?
+
+x = "10"
+y = int(x) + 5
+print(y)
+
+# OUTPUT:
+# 15
+
+# Q) What will be the output of the following code?
+
+count =  5
+count += 1
+print(count)
+
+# OUTPUT:
+# 6
+
+# Q) What will be the output of the following code?
+
+is_python_fun = True
+is_sky_blue = False
+print(is_sky_blue)
+print(is_python_fun)
+
+# OUTPUT:
+# False
+# True
+
+# Q) What will be the output of the following code?
+
+x = 10
+del x
+print(x)
+
+# OUTPUT:
+# NameError: name 'x' is not defined
+
