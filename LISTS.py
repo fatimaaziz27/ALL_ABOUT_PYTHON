c8ebac3ee0514beb85ef94e3ee3b8df2ae26ea18
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-01-04 23:13:32 +0500

    fixed

--- LISTS.py
+++ LISTS.py
@@ -48,3 +48,106 @@ print(a)
 # OUTPUT:
 # [1, 2, 3, 4]
 # Q) What will be the output of the following code?
+my_list = [10, 20, 30, 40, 50]
+print(my_list[-1])
+# OUTPUT:
+# 50
+# Q) What will be the output of the following code?
+letters = ['a', 'b', 'c', 'd']
+print(letters[1:3])
+# OUTPUT:
+# ['b', 'c']
+# Q) What will be the output of the following code?
+nums = [1, 2, 3, 4, 5]
+nums.clear()
+print(nums)
+# OUTPUT:
+# []
+# Q) What will be the output of the following code?
+my_list = [1, 2, 3, 4, 5]
+print(sum(my_list))
+# OUTPUT:
+# 15
+# Q) What will be the output of the following code?
+items = ['pen', 'pencil', 'eraser']
+items.insert(1, 'marker')
+print(items)
+# OUTPUT:
+# ['pen', 'marker', 'pencil', 'eraser']
+# Q) What will be the output of the following code?
+numbers = [3, 1, 4, 1, 5]
+numbers.sort()
+print(numbers)
+# OUTPUT:
+# [1, 1, 3, 4, 5]
+# Q) What will be the output of the following code?
+a = [1, 2, 3]
+b = [4, 5, 6]
+combined = a + b
+print(combined)
+# OUTPUT:
+# [1, 2, 3, 4, 5, 6]
+# Q) What will be the output of the following code?
+my_list = [10, 20, 30, 40, 50]
+print(my_list.index(30))
+# OUTPUT:
+# 2
+# Q) What will be the output of the following code?
+fruits = ["apple", "banana", "cherry", "date"]
+print("banana" in fruits)
+# OUTPUT:
+# True
+# Q) What will be the output of the following code?
+numbers = [1, 2, 3, 4, 5]
+print(numbers.count(3))
+# OUTPUT:
+# 1
+# Q) What will be the output of the following code?
+my_list = [1, 2, 3, 4, 5]
+reversed_list = my_list[::-1]
+print(reversed_list)
+# OUTPUT:
+# [5, 4, 3, 2, 1]
+# Q) What will be the output of the following code?
+a = [1, 2, 3]
+b = a.copy()
+b.append(4)
+print(a)
+# OUTPUT:
+# [1, 2, 3]
+# Q) What will be the output of the following code?
+numbers = [10, 20, 30, 40, 50]
+del numbers[2]
+print(numbers)
+# OUTPUT:
+# [10, 20, 40, 50]
+# Q) What will be the output of the following code?
+my_list = [1, 2, 3, 4, 5]
+print(min(my_list))
+# OUTPUT:
+# 1
+# Q) What will be the output of the following code?
+values = [5, 10, 15, 20]
+average = sum(values) / len(values)
+print(average)
+# OUTPUT:
+# 12.5
+# Q) What will be the output of the following code?
+
+chars = ['a', 'b', 'c', 'd']
+chars.pop()
+print(chars)
+# OUTPUT:
+# ['a', 'b', 'c']
+# Q) What will be the output of the following code?
+my_list = [2, 4, 6, 8, 10]
+even_numbers = [x for x in my_list if x % 2 == 0]
+print(even_numbers)
+# OUTPUT:
+# [2, 4, 6, 8, 10]
+# Q) What will be the output of the following code?
+items = ['a', 'b', 'c', 'd']
+items.extend(['e', 'f'])
+print(items)
+# OUTPUT:
+# ['a', 'b', 'c', 'd', 'e', 'f']
