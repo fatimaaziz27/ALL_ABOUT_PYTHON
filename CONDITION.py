92d770ef9dbb4cc50493217beda0f3f6455fdbeb
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-01-11 23:44:58 +0500

    fixi

---
+++ CONDITION.py
@@ -0,0 +1,15 @@
+# Q1) What will be the output of the following code?
+
+age=20
+if age >= 18:
+    if age >= 65:
+        print("senior citizen")
+    else:
+        print("Adult")
+else:
+    print("Minor")
+
+# OUTPUT:
+# Adult
+
+# Q2) What will be the output of the following code?
