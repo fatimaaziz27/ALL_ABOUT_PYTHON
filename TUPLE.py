ee7a34acf872def1a8842098eae0658a53bd6064
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-01-20 22:18:01 +0500

    f

---
+++ TUPLE.py
@@ -0,0 +1,31 @@
+# Q1) what will be the output of the following code?
+
+my_tuple = (1, 2, 3, 4, 5)
+if 3 in my_tuple:
+    print("3 is present")
+else:
+    print("3 is missing")
+
+# OUTPUT:
+# 3 is present
+
+# Q2) what will be the output of the following code?
+
+t = (1,2,3)
+t[1] = 10
+print(t)
+
+# OUTPUT:
+# TypeError: 'tuple' object does not support item assignment
+
+# Q3) what will be the output of the following code?
+
+t = (5,)
+print(type(t))
+
+# OUTPUT:
+# <class 'tuple'>
+
+# Q4) what will be the output of the following code?
+
+t = (1, 2, 3, 4, 5)
