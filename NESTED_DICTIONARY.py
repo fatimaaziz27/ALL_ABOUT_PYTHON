7788632bd90213d8e7495de8de1ce1f1d60d0701
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-01-24 23:56:05 +0500

    f

--- NESTED_DICTIONARY.py
+++ NESTED_DICTIONARY.py
@@ -3,3 +3,9 @@
 # Q1) what will be the output of the following code?
 
 student = {"alice":{"age": 20,"marks": 45} , "bob":{"age": 22,"marks": 50}}
+print(student["alice"]["marks"])
+
+# OUTPUT:
+# 45
+
+# Q2) what will be the output of the following code?
