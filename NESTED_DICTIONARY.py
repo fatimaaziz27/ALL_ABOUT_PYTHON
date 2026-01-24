a7c045f05c8e1a2a865171863a7e1fccf4dfaf17
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-01-24 23:55:23 +0500

    fixo

--- NESTED_DICTIONARY.py
+++ NESTED_DICTIONARY.py
@@ -2,9 +2,4 @@
 
 # Q1) what will be the output of the following code?
 
-student = {
-    "alice":{
-        "age": 20,
-        "marks": 45
-    },
-    "bob":{
+student = {"alice":{"age": 20,"marks": 45} , "bob":{"age": 22,"marks": 50}}
