5c4927225a55de8bc02ad2ec798704111784cfd9
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-01-24 23:54:02 +0500

    fixi

--- NESTED_DICTIONARY.py
+++ NESTED_DICTIONARY.py
@@ -1,3 +1,10 @@
 #                                  Nested Dictionary
 
 # Q1) what will be the output of the following code?
+
+student = {
+    "alice":{
+        "age": 20,
+        "marks": 45
+    },
+    "bob":{
