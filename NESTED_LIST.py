3e34685b900aa62aee127789811c4146f9cdc784
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-02-28 20:33:54 +0500

    fixi

---
+++ NESTED_LIST.py
@@ -0,0 +1,21 @@
+# Q1) What will be the output of the following code?
+
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+print(matrix[1][2])
+
+# OUTPUT:
+# 6
+
+# Q1) What will be the output of the following code?
+
+# names = [["Alice","bob","charlie"],["david","eve","frank"],["grace","hank","ivy"]]
+# print(names[1][2])
+
+# OUTPUT:
+# frank
+
+# Q1) What will be the output of the following code?
+
+names = [["john","jane","jim"],["tom","tim","tina"],["sarah","sam","sophie"]]
+print(names[2])
+
