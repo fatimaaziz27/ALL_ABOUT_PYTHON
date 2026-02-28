2a0936b3ecd3d274c36ec349d06885fefbccbcb1
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-02-28 20:40:20 +0500

    fix

---
+++ MIXED_LIST_&_DICTIONARY.py
@@ -0,0 +1,19 @@
+# Q1) What will be the output of the following code?
+
+students = [
+    {"name":"sara","age": 18},
+    {"name":"yusra","age":14}
+]
+print(students[1]["name"])
+
+# OUTPUT:
+# yusra
+
+# Q1) What will be the output of the following code?
+
+products = [
+    {"item":"laptop","price":800},
+    {"item":"phone","price":500}
+]
+products[0]["price"]=750
+print(products)
