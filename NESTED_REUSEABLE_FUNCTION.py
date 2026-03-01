4638eafd1c957db0ad85d7bf7c8c4f9f6f21d73e
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-03-01 20:49:02 +0500

    fixed

--- NESTED_REUSEABLE_FUNCTION.py
+++ NESTED_REUSEABLE_FUNCTION.py
@@ -7,9 +7,10 @@ print(is_even(4))
 # OUTPUT:
 # True
 
-
 # Q) What will be the output of the following code?
 
-
+def reverse_string(s):
+    return s[::-1]
+print(reverse_string("hello"))
 
 # Q) What will be the output of the following code?
