b69bed79ac9475eb4afd58c63902855ae2e87423
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-03-01 20:47:38 +0500

    fix

---
+++ NESTED_REUSEABLE_FUNCTION.py
@@ -0,0 +1,14 @@
+# Q) What will be the output of the following code?
+
+def is_even(num):
+    return num%2==0
+print(is_even(4))
+
+# OUTPUT:
+
+
+# Q) What will be the output of the following code?
+
+
+
+# Q) What will be the output of the following code?
