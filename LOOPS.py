2c4e10399c8a0cab6ec579944300404571c4735c
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-04-07 08:29:10 +0500

    Update LOOPS.py

--- LOOPS.py
+++ LOOPS.py
@@ -43,17 +43,17 @@ print(total)
 # Output:
 # 55
 
-Q6
+# Q6
 for i in range(1, 10):
     if i == 5:
         break
     print(i)
 
-Output:
-1
-2
-3
-4
+# Output:
+# 1
+# 2
+# 3
+# 4
 
 Q7
 for i in range(1, 6):
