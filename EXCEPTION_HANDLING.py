87f0b763f40c5640a20ecd934a4e553968c76f45
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-04-18 08:23:11 +0500

    Update EXCEPTION_HANDLING.py

--- EXCEPTION_HANDLING.py
+++ EXCEPTION_HANDLING.py
@@ -1,1 +1,12 @@
 #                                   Exception Handling
+
+# Division Error (ZeroDivisionError)
+
+try:
+   a = int(input("Enter first number: "))
+   b = int(input("Enter second number: "))
+  
+   result = a / b
+   print("Result is:", result)
+except ZeroDivisionError:
+   print("Cannot divide by zero!")
