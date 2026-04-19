88f4bdede1cd1c2409fa8a65dae65d0e13ebb51f
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-04-20 01:06:04 +0500

    Update LOOPS.py

--- LOOPS.py
+++ LOOPS.py
@@ -288,3 +288,68 @@ for i in range(len(players)):
 # Team: Player2 vs Player3
 # Team: Player2 vs Player4
 # Team: Player3 vs Player4
+
+# Q) Generate full multiplication tables from 2–5. 
+
+# CODE:
+
+i=2
+while i <= 5:
+    print(f"TABLE OF {i}")
+    j=1
+    while j<=10:
+        print(f"{i} x {j} = {i*j}")
+        j+=1
+    print()
+    i+=1    
+
+# OUTPUT:
+
+# TABLE OF 2
+# 2 x 1 = 2
+# 2 x 2 = 4
+# 2 x 3 = 6
+# 2 x 4 = 8
+# 2 x 5 = 10
+# 2 x 6 = 12
+# 2 x 7 = 14
+# 2 x 8 = 16
+# 2 x 9 = 18
+# 2 x 10 = 20
+
+# TABLE OF 3
+# 3 x 1 = 3
+# 3 x 2 = 6
+# 3 x 3 = 9
+# 3 x 4 = 12
+# 3 x 5 = 15
+# 3 x 6 = 18
+# 3 x 7 = 21
+# 3 x 8 = 24
+# 3 x 9 = 27
+# 3 x 10 = 30
+
+# TABLE OF 4
+# 4 x 1 = 4
+# 4 x 2 = 8
+# 4 x 3 = 12
+# 4 x 4 = 16
+# 4 x 5 = 20
+# 4 x 6 = 24
+# 4 x 7 = 28
+# 4 x 8 = 32
+# 4 x 9 = 36
+# 4 x 10 = 40
+
+# TABLE OF 5
+# 5 x 1 = 5
+# 5 x 2 = 10
+# 5 x 3 = 15
+# 5 x 4 = 20
+# 5 x 5 = 25
+# 5 x 6 = 30
+# 5 x 7 = 35
+# 5 x 8 = 40
+# 5 x 9 = 45
+# 5 x 10 = 50
+
