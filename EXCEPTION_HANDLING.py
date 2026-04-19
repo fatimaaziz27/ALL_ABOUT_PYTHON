bc90d873757f5e759871461909574467cf6060f2
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-04-19 13:54:01 +0500

    Add multiple exception handling for division

--- EXCEPTION_HANDLING.py
+++ EXCEPTION_HANDLING.py
@@ -10,3 +10,14 @@ try:
    print("Result is:", result)
 except ZeroDivisionError:
    print("Cannot divide by zero!")
+   
+# Multiple Exceptions
+
+try:
+   num = int(input("Enter a number: "))
+   result = 10 / num
+   print("Result:", result)
+except ValueError:
+   print("Invalid number!")
+except ZeroDivisionError:
+   print("Cannot divide by zero!")
