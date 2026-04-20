d7fb0432fb0cf3dd58e278ddd37342d82950a195
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-04-20 19:41:08 +0500

    Implement flatten function for nested lists
    Added a function to flatten a nested list using loops.

--- LOOPS.py
+++ LOOPS.py
@@ -449,3 +449,17 @@ print(remove_duplicates(lst))  # [1, 2, 3, 4, 5, 6]
 
 # OUTPUT:
 # [1, 2, 3, 4, 5, 6]
+
+# Q) Flatten a nested list using loop. 
+
+def flatten(nested_lst):
+    flat_lst = []
+    for sublist in nested_lst:
+        for item in sublist:
+            flat_lst.append(item)
+    return flat_lst
+nested_lst = [[1, 2], [3, 4], [5, 6]]
+print(flatten(nested_lst)) 
+
+# OUTPUT:
+# [1, 2, 3, 4, 5, 6]
