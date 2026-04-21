e93579714cf69fa57b0ea9a7418c257f7c9bb31e
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-04-22 03:17:08 +0500

    Update practice_questions.py

--- practice_questions.py
+++ practice_questions.py
@@ -103,37 +103,7 @@ print_area(square)
 # The area of the shape is: 78.53975
 # The area of the shape is: 16
 
-# Q) A machine that takes user input for product name and tells the price of the product if it's available in the list.
 
-# ANSWER:
-
-products = {
-    "Apple": 100,
-    "Banana": 50,
-    "Orange": 75,
-    "Mango": 200,
-    "Grapes": 150
-}
-def get_product_price(product_name):
-    if product_name in products:
-        return f"The price of {product_name} is {products[product_name]} PKR"
-    else:
-        return f"Sorry, {product_name} is not available or not for sale."
-while True:
-    product_name = input("Enter product name (or 'quit' to stop): ")
-    if product_name.lower() == 'quit':
-        break
-    print(get_product_price(product_name.capitalize()))
-
-# OUTPUT:
-
-# Enter product name (or 'quit' to stop): apple
-# The price of Apple is 100 PKR
-# Enter product name (or 'quit' to stop): mango
-# The price of Mango is 200 PKR
-# Enter product name (or 'quit' to stop): peach
-# Sorry, Peach is not available or not for sale.
-# Enter product name (or 'quit' to stop): quit
 
 # Q) Remove duplicate characters from string (no set).
 
