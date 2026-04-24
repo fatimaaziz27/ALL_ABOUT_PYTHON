454210f24876f37903ca04798cfefb2c3429f985
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-04-25 00:54:25 +0500

    Update FILE_HANDLING.py

--- FILE_HANDLING.py
+++ FILE_HANDLING.py
@@ -1,1 +1,11 @@
-#                                FILE HANDLING
+#             FILE HANDLING
+
+# Append to File
+
+file = open("test.txt", "a")
+file.write("\nThis is new added text")
+file.close()
+
+file = open("test.txt", "r")
+print(file.read())
+file.close()
