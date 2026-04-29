71ed2a1ec0d213d7f561306f0c98a8716940fd7c
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-04-29 23:24:32 +0500

    Update I_O_PROGRAMS.py

--- I_O_PROGRAMS.py
+++ I_O_PROGRAMS.py
@@ -4,3 +4,11 @@
 
 color = input("Enter your favorite color: ")
 print("Your favorite color is", color)
+
+# Two Numbers Addition
+
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+sum = int(num1) + int(num2)
+print("The sum is", sum)
+
