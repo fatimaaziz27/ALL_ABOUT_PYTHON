42f30b3fc7acb5a70dd8c2bdadb74de40485c156
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-05-04 02:03:07 +0500

    Fix formatting issues in OPERATIONS.py

--- OPERATIONS.py
+++ OPERATIONS.py
@@ -17,9 +17,9 @@ print("Not Equal:", p != q)
 print("Logical NOT:", not(p > q))
 
 Modulus & OR
+
 num1 = 17
 num2 = 5
-
 print("Remainder:", num1 % num2)
 print("Greater or Equal:", num1 >= num2)
 print("Logical OR:", (num1 < 10 or num2 < 10))
