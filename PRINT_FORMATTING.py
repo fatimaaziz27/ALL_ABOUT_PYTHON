461ef9aa83a126486a49b4a6645fe749aee33715
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-05-06 01:38:19 +0500

    Update PRINT_FORMATTING.py

--- PRINT_FORMATTING.py
+++ PRINT_FORMATTING.py
@@ -1,6 +1,14 @@
 # Print Formatting (Output)
 
 # Age Display
+
 name = "Sara"
 age = 18
 print(f"{name} is {age} years old")
+
+# Price and Quantity
+
+item = "Book"
+price = 500
+quantity = 2
+print(f"You bought {quantity} {item}s for {price} each")
