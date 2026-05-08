374a02abbb5587037783703a456427082f6e6255
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-05-08 09:05:45 +0500

    Update PRINT_FORMATTING.py

--- PRINT_FORMATTING.py
+++ PRINT_FORMATTING.py
@@ -19,3 +19,9 @@ name = "Ahmed"
 marks = 92
 grade = "A"
 print(f"{name} scored {marks} marks and got grade {grade}")
+
+# Temperature Display
+
+city = "Karachi"
+temp = 35
+print(f"The temperature in {city} is {temp}°C")
