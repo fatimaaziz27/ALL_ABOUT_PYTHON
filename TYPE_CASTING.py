7636ffaa97087ddb8f8b15e1112af5e2f7d6e803
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-05-09 05:55:09 +0500

    Update TYPE_CASTING.py

--- TYPE_CASTING.py
+++ TYPE_CASTING.py
@@ -14,9 +14,8 @@ b = input("Enter another number: ")
 product = int(a) * int(b)
 print("Product is:", product)
 
-String to Integer (Age Next Year)
+# String to Integer (Age Next Year)
+
 age = input("Enter your age: ")
-
 next_age = int(age) + 1
-
 print("Next year your age will be:", next_age)
