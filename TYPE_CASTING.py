9a96c32e792bca50df530c2ed318e3954f5cd026
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-05-10 08:27:15 +0500

    Add output comments for type casting examples

--- TYPE_CASTING.py
+++ TYPE_CASTING.py
@@ -6,12 +6,10 @@ num1 = input("Enter first number: ")
 num2 = input("Enter second number: ")
 result = float(num1) + float(num2)
 print("Sum is:", result)
-
 # OUTPUT:
-
-Enter first number: 5
-Enter second number: 4
-Sum is: 9.0
+# Enter first number: 5
+# Enter second number: 4
+# Sum is: 9.0
 
 # Integer Multiplication
 
@@ -19,12 +17,14 @@ a = input("Enter a number: ")
 b = input("Enter another number: ")
 product = int(a) * int(b)
 print("Product is:", product)
+# OUTPUT:
 
 # String to Integer (Age Next Year)
 
 age = input("Enter your age: ")
 next_age = int(age) + 1
 print("Next year your age will be:", next_age)
+# OUTPUT:
 
 # Float Division
 
@@ -32,10 +32,11 @@ x = input("Enter first number: ")
 y = input("Enter second number: ")
 division = float(x) / float(y)
 print("Result is:", division)
+# OUTPUT:
 
 # Boolean Type Casting
 
 num = input("Enter a number: ")
 result = bool(int(num))
 print("Boolean value is:", result)
-
+# OUTPUT:
