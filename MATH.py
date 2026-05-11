e11e98b7eb9827218acc668d29d457c29b72d575
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-05-12 00:39:31 +0500

    Update MATH.py

--- MATH.py
+++ MATH.py
@@ -3,8 +3,13 @@
 # Ceiling and Floor
 
 import math
-
 num = 4.7
 print("Ceiling:", math.ceil(num))
 print("Floor:", math.floor(num))
 
+Factorial
+import math
+
+num = 5
+
+print("Factorial:", math.factorial(num))
