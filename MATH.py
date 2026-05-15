08f1bb3cda6f63b85fba70cc17e6517c13e180c5
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-05-15 12:01:20 +0500

    Update MATH.py

--- MATH.py
+++ MATH.py
@@ -1,8 +1,8 @@
 #                   MATH MODULE
 
-# Ceiling and Floor
-
 import math
+
+# Ceiling and Floor
 
 num = 4.7
 print("Ceiling:", math.ceil(num))
@@ -10,14 +10,10 @@ print("Floor:", math.floor(num))
 
 # Factorial
 
-import math
-
 num = 5
 print("Factorial:", math.factorial(num))
 
 #Value of Pi and Area of Circle
-
-import math
 
 radius = 3
 area = math.pi * radius ** 2
